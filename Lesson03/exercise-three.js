33f1029e1cecbfa7e88c79f9cf06457e3c906fbe
aimzOG <aimz93@gmail.com>
2019-04-18 18:31:37 -0700

    adds exercise two and three

---
+++ Lesson03/exercise-three.js
@@ -0,0 +1,5 @@
+const phrase = ["hakuna", "matata", "it", "means", "no", "worries"];
+
+phrase.forEach(function (word, i) {
+  console.log(i + ", " + word)
+});
---
+++ Lesson03/exercise-two.js
@@ -0,0 +1,6 @@
+const sentence = 'I would tell you a chemistry joke but I would not get a reaction.';
+const sentenceWords = sentence.split(' ');
+
+console.log(sentenceWords); // to show it does in fact make an array
+console.log(sentenceWords[2]); // retrieves 3rd word in sentence 'tell'
+
