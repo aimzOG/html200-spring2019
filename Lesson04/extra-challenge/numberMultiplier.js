8cdb58db23e260db60051ce45f117e0fbcd94a77
aimzOG <aimz93@gmail.com>
2019-04-28 00:48:04 -0700

    creates new dir, adds, extra-challenge

---
+++ Lesson04/extra-challenge/numberMultiplier.js
@@ -0,0 +1,9 @@
+function numberMultiplier() {
+  let inputA = prompt('Enter a number: ');
+  let inputB = prompt('Enter another number: ');
+  let result = 0;
+
+  result = Number(inputA) * Number(inputB);
+
+  alert(result);
+}
